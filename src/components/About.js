7ac6f7d054df58b363f3848acd2d147b80efc167
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-12 20:28:38 -0700

    complete lab

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+export default function About({
+    about, 
+    image = "https://via.placeholder.com/215"
+}) {
+    return(
+        <aside>
+            <img alt= "blog logo" src={image} ></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
